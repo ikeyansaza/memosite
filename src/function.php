652a0d7364175b3994a8308f036ea82f424659b5
ikeyan <abetyan31503@gmail.com>
2020-10-24 21:48:26 +0900

    docker環境を追加

---
+++ src/function.php
@@ -0,0 +1,532 @@
+<?php
+//================================
+// ログ
+//================================
+//ログを取るか
+ini_set('log_errors', 'on');
+//ログの出力ファイルを指定
+ini_set('error_log', 'php.log');
+
+//================================
+// デバッグ
+//================================
+//デバッグフラグ
+$debug_flg = false;
+//デバッグログ関数
+function debug($str)
+{
+    global $debug_flg;
+    if (!empty($debug_flg)) {
+        error_log('デバッグ：'.$str);
+    }
+}
+
+//================================
+// セッション準備・セッション有効期限を延ばす
+//================================
+//セッションファイルの置き場を変更する（/var/tmp/以下に置くと30日は削除されない）
+session_save_path("/var/tmp/");
+//ガーベージコレクションが削除するセッションの有効期限を設定（30日以上経っているものに対してだけ１００分の１の確率で削除）
+ini_set('session.gc_maxlifetime', 60*60*24*30);
+//ブラウザを閉じても削除されないようにクッキー自体の有効期限を延ばす
+ini_set('session.cookie_lifetime ', 60*60*24*30);
+//セッションを使う
+session_start();
+//現在のセッションIDを新しく生成したものと置き換える（なりすましのセキュリティ対策）
+session_regenerate_id();
+
+//================================
+// 画面表示処理開始ログ吐き出し関数
+//================================
+function debugLogStart()
+{
+    debug('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> 画面表示処理開始');
+    debug('セッションID：'.session_id());
+    debug('セッション変数の中身：'.print_r($_SESSION, true));
+    debug('現在日時タイムスタンプ：'.time());
+    if (!empty($_SESSION['login_date']) && !empty($_SESSION['login_limit'])) {
+        debug('ログイン期限日時タイムスタンプ：'.($_SESSION['login_date'] + $_SESSION['login_limit']));
+    }
+}
+
+//================================
+// 定数
+//================================
+//エラーメッセージを定数に設定
+define('MSG01', '入力必須です');
+define('MSG02', 'Emailの形式で入力してください');
+define('MSG03', 'パスワード（再入力）が合っていません');
+define('MSG04', '半角英数字のみご利用いただけます');
+define('MSG05', '6文字以上で入力してください');
+define('MSG06', '256文字以内で入力してください');
+define('MSG07', 'エラーが発生しました。しばらく経ってからやり直してください。');
+define('MSG08', 'そのEmailは既に登録されています');
+define('MSG09', 'メールアドレスまたはパスワードが違います');
+define('MSG10', '電話番号の形式が違います');
+define('MSG12', '古いパスワードが違います');
+define('MSG13', '古いパスワードと同じです');
+define('MSG14', '文字で入力してください');
+define('MSG15', '正しくありません');
+define('MSG16', '有効期限が切れています');
+define('MSG17', '半角数字のみご利用いただけます');
+define('SUC01', 'パスワードを変更しました');
+define('SUC02', 'プロフィールを変更しました');
+define('SUC03', 'メールを送信しました');
+define('SUC04', '登録しました');
+
+//================================
+// グローバル変数
+//================================
+//エラーメッセージ格納用の配列
+$err_msg = array();
+//================================
+// バリデーション関数
+//================================
+
+//バリデーション関数（未入力チェック）
+function validRequired($str, $key)
+{
+    if ($str === '') { //金額フォームなどを考えると数値の０はOKにし、空文字はダメにする
+        global $err_msg;
+        $err_msg[$key] = MSG01;
+    }
+}
+//バリデーション関数（Email形式チェック）
+function validEmail($str, $key)
+{
+    if (!preg_match("/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/", $str)) {
+        global $err_msg;
+        $err_msg[$key] = MSG02;
+    }
+}
+//バリデーション関数（Email重複チェック）
+function validEmailDup($email)
+{
+    global $err_msg;
+    //例外処理
+    try {
+        // DBへ接続
+        $dbh = dbConnect();
+        // SQL文作成
+        $sql = 'SELECT count(*) FROM users WHERE email = :email AND delete_flg = 0';
+        $data = array(':email' => $email);
+        // クエリ実行
+        $stmt = queryPost($dbh, $sql, $data);
+        // クエリ結果の値を取得
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        //array_shift関数は配列の先頭を取り出す関数です。クエリ結果は配列形式で入っているので、array_shiftで1つ目だけ取り出して判定します
+        if (!empty(array_shift($result))) {
+            $err_msg['email'] = MSG08;
+        }
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+        $err_msg['common'] = MSG07;
+    }
+}
+//バリデーション関数（同値チェック）
+function validMatch($str1, $str2, $key)
+{
+    if ($str1 !== $str2) {
+        global $err_msg;
+        $err_msg[$key] = MSG03;
+    }
+}
+//バリデーション関数（最小文字数チェック）
+function validMinLen($str, $key, $min = 6)
+{
+    if (mb_strlen($str) < $min) {
+        global $err_msg;
+        $err_msg[$key] = MSG05;
+    }
+}
+//バリデーション関数（最大文字数チェック）
+function validMaxLen($str, $key, $max = 255)
+{
+    if (mb_strlen($str) > $max) {
+        global $err_msg;
+        $err_msg[$key] = MSG06;
+    }
+}
+//バリデーション関数（半角チェック）
+function validHalf($str, $key)
+{
+    if (!preg_match("/^[a-zA-Z0-9]+$/", $str)) {
+        global $err_msg;
+        $err_msg[$key] = MSG04;
+    }
+}
+//半角数字チェック
+function validNumber($str, $key)
+{
+    if (!preg_match("/^[0-9]+$/", $str)) {
+        global $err_msg;
+        $err_msg[$key] = MSG17;
+    }
+}
+//固定長チェック
+function validLength($str, $key, $len = 8)
+{
+    if (mb_strlen($str) !== $len) {
+        global $err_msg;
+        $err_msg[$key] = $len . MSG14;
+    }
+}
+//パスワードチェック
+function validPass($str, $key)
+{
+    //半角英数字チェック
+    validHalf($str, $key);
+    //最大文字数チェック
+    validMaxLen($str, $key);
+    //最小文字数チェック
+    validMinLen($str, $key);
+}
+//selectboxチェック
+function validSelect($str, $key)
+{
+    if (!preg_match("/^[0-9]+$/", $str)) {
+        global $err_msg;
+        $err_msg[$key] = MSG15;
+    }
+}
+//エラーメッセージ表示
+function getErrMsg($key)
+{
+    global $err_msg;
+    if (!empty($err_msg[$key])) {
+        return $err_msg[$key];
+    }
+}
+
+//================================
+// データベース
+//================================
+//DB接続関数
+function dbConnect()
+{
+    //DBへの接続準備
+    $dsn = 'mysql:dbname=test;host=localhost;charset=utf8';
+    $user = 'test';
+    $password = 'test';
+    $options = array(
+    // SQL実行失敗時にはエラーコードのみ設定
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_SILENT,
+    // デフォルトフェッチモードを連想配列形式に設定
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    // バッファードクエリを使う(一度に結果セットをすべて取得し、サーバー負荷を軽減)
+    // SELECTで得た結果に対してもrowCountメソッドを使えるようにする
+    PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true,
+  );
+    error_log('ここ2');
+
+    // PDOオブジェクト生成（DBへ接続）
+    $dbh = new PDO($dsn, $user, $password, $options);
+    error_log('ここ3');
+    return $dbh;
+}
+//SQL実行関数
+function queryPost($dbh, $sql, $data)
+{
+    //クエリー作成
+    $stmt = $dbh->prepare($sql);
+    //プレースホルダに値をセットし、SQL文を実行
+    if (!$stmt->execute($data)) {
+        debug('クエリに失敗しました。');
+        debug('失敗したSQL：'.print_r($stmt, true));
+        $err_msg['common'] = MSG07;
+        return 0;
+    }
+    debug('クエリ成功。');
+    return $stmt;
+}
+//ユーザー情報を取得
+function getUser($u_id)
+{
+    debug('ユーザー情報を取得します。');
+    //例外処理
+    try {
+        // DBへ接続
+        $dbh = dbConnect();
+        // SQL文作成
+        $sql = 'SELECT * FROM users  WHERE id = :u_id AND delete_flg = 0';
+        $data = array(':u_id' => $u_id);
+        // クエリ実行
+        $stmt = queryPost($dbh, $sql, $data);
+
+        // クエリ結果のデータを１レコード返却
+        if ($stmt) {
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            return false;
+        }
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+    }
+}
+
+//================================
+// その他
+//================================
+// サニタイズ
+function sanitize($str)
+{
+    return htmlspecialchars($str, ENT_QUOTES);
+}
+// フォーム入力保持
+function getFormData($str, $flg = false)
+{
+    if ($flg) {
+        $method = $_GET;
+    } else {
+        $method = $_POST;
+    }
+    global $dbFormData;
+    // ユーザーデータがある場合
+    if (!empty($dbFormData)) {
+        //フォームのエラーがある場合
+        if (!empty($err_msg[$str])) {
+            //POSTにデータがある場合
+            if (isset($method[$str])) {
+                return sanitize($method[$str]);
+            } else {
+                //ない場合（基本ありえない）はDBの情報を表示
+                return sanitize($dbFormData[$str]);
+            }
+        } else {
+            //POSTにデータがあり、DBの情報と違う場合
+            if (isset($method[$str]) && $method[$str] !== $dbFormData[$str]) {
+                return sanitize($method[$str]);
+            } else {
+                return sanitize($dbFormData[$str]);
+            }
+        }
+    } else {
+        if (isset($method[$str])) {
+            return sanitize($method[$str]);
+        }
+    }
+}
+// 画像処理
+function uploadImg($file, $key)
+{
+    debug('画像アップロード処理開始');
+    debug('FILE情報：'.print_r($file, true));
+
+    if (isset($file['error']) && is_int($file['error'])) {
+        try {
+            // バリデーション
+            // $file['error'] の値を確認。配列内には「UPLOAD_ERR_OK」などの定数が入っている。
+            //「UPLOAD_ERR_OK」などの定数はphpでファイルアップロード時に自動的に定義される。定数には値として0や1などの数値が入っている。
+            switch ($file['error']) {
+          case UPLOAD_ERR_OK: // OK
+              break;
+          case UPLOAD_ERR_NO_FILE:   // ファイル未選択の場合
+              throw new RuntimeException('ファイルが選択されていません');
+          case UPLOAD_ERR_INI_SIZE:  // php.ini定義の最大サイズが超過した場合
+          case UPLOAD_ERR_FORM_SIZE: // フォーム定義の最大サイズ超過した場合
+              throw new RuntimeException('ファイルサイズが大きすぎます');
+          default: // その他の場合
+              throw new RuntimeException('その他のエラーが発生しました');
+      }
+
+            // $file['mime']の値はブラウザ側で偽装可能なので、MIMEタイプを自前でチェックする
+            // exif_imagetype関数は「IMAGETYPE_GIF」「IMAGETYPE_JPEG」などの定数を返す
+            $type = @exif_imagetype($file['tmp_name']);
+            if (!in_array($type, [IMAGETYPE_GIF, IMAGETYPE_JPEG, IMAGETYPE_PNG], true)) { // 第三引数にはtrueを設定すると厳密にチェックしてくれるので必ずつける
+                throw new RuntimeException('画像形式が未対応です');
+            }
+
+            // ファイルデータからSHA-1ハッシュを取ってファイル名を決定し、ファイルを保存する
+            // ハッシュ化しておかないとアップロードされたファイル名そのままで保存してしまうと同じファイル名がアップロードされる可能性があり、
+            // DBにパスを保存した場合、どっちの画像のパスなのか判断つかなくなってしまう
+            // image_type_to_extension関数はファイルの拡張子を取得するもの
+            $path = 'uploads/'.sha1_file($file['tmp_name']).image_type_to_extension($type);
+            if (!move_uploaded_file($file['tmp_name'], $path)) { //ファイルを移動する
+                throw new RuntimeException('ファイル保存時にエラーが発生しました');
+            }
+            // 保存したファイルパスのパーミッション（権限）を変更する
+            chmod($path, 0644);
+
+            debug('ファイルは正常にアップロードされました');
+            debug('ファイルパス：'.$path);
+            return $path;
+        } catch (RuntimeException $e) {
+            debug($e->getMessage());
+            global $err_msg;
+            $err_msg[$key] = $e->getMessage();
+        }
+    }
+}
+
+//================================
+// メール送信
+//================================
+function sendMail($from, $to, $subject, $comment)
+{
+    if (!empty($to) && !empty($subject) && !empty($comment)) {
+        //文字化けしないように設定（お決まりパターン）
+        mb_language("Japanese"); //現在使っている言語を設定する
+        mb_internal_encoding("UTF-8"); //内部の日本語をどうエンコーディング（機械が分かる言葉へ変換）するかを設定
+
+        //メールを送信（送信結果はtrueかfalseで返ってくる）
+        $result = mb_send_mail($to, $subject, $comment, "From: ".$from);
+        //送信結果を判定
+        if ($result) {
+            debug('メールを送信しました。');
+        } else {
+            debug('【エラー発生】メールの送信に失敗しました。');
+        }
+    }
+}
+//認証キー生成
+function makeRandKey($length = 8)
+{
+    static $chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJLKMNOPQRSTUVWXYZ0123456789';
+    $str = '';
+    for ($i = 0; $i < $length; ++$i) {
+        $str .= $chars[mt_rand(0, 61)];
+    }
+    return $str;
+}
+//memoEdit.phpにて使用
+function getMemoOne($p_id)
+{
+    debug('メモ情報を取得します。');
+    debug('メモID：'.$p_id);
+    //例外処理
+    try {
+        // DBへ接続
+        $dbh = dbConnect();
+        // SQL文作成
+        $sql = 'SELECT * FROM memo WHERE id = :p_id AND delete_flg = 0';
+        $data = array(':p_id' => $p_id);
+        // クエリ実行
+        $stmt = queryPost($dbh, $sql, $data);
+
+        if ($stmt) {
+            // クエリ結果のデータを１レコード返却
+            return $stmt->fetch(PDO::FETCH_ASSOC);
+        } else {
+            return false;
+        }
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+    }
+}
+//GETパラメータ付与
+// $del_key : 付与から取り除きたいGETパラメータのキー
+function appendGetParam($arr_del_key = array())
+{
+    if (!empty($_GET)) {
+        $str = '?';
+        foreach ($_GET as $key => $val) {
+            if (!in_array($key, $arr_del_key, true)) { //取り除きたいパラメータじゃない場合にurlにくっつけるパラメータを生成
+                $str .= $key.'='.$val.'&';
+            }
+        }
+        $str = mb_substr($str, 0, -1, "UTF-8");
+        return $str;
+    }
+}
+function isLike($u_id, $p_id)
+{
+    debug('お気に入り情報があるか確認します。');
+    debug('ユーザーID：'.$u_id);
+    debug('商品ID：'.$p_id);
+    //例外処理
+    try {
+        // DBへ接続
+        $dbh = dbConnect();
+        // SQL文作成
+        $sql = 'SELECT * FROM favorite WHERE memo_id = :p_id AND user_id = :u_id';
+        $data = array(':u_id' => $u_id, ':p_id' => $p_id);
+        // クエリ実行
+        $stmt = queryPost($dbh, $sql, $data);
+
+        if ($stmt->rowCount()) {
+            debug('お気に入りです');
+            return true;
+        } else {
+            debug('特に気に入ってません');
+            return false;
+        }
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+    }
+}
+//================================
+// ログイン認証
+//================================
+function isLogin()
+{
+    // ログインしている場合
+    if (!empty($_SESSION['login_date'])) {
+        debug('ログイン済みユーザーです。');
+
+        // 現在日時が最終ログイン日時＋有効期限を超えていた場合
+        if (($_SESSION['login_date'] + $_SESSION['login_limit']) < time()) {
+            debug('ログイン有効期限オーバーです。');
+
+            // セッションを削除（ログアウトする）
+            session_destroy();
+            return false;
+        } else {
+            debug('ログイン有効期限以内です。');
+            return true;
+        }
+    } else {
+        debug('未ログインユーザーです。');
+        return false;
+    }
+}
+
+//index.php表示用のメモリスト
+function getMemoList($currentMinNum = 1, $listSpan = 5, $u_id)
+{
+    debug('商品情報を取得します。');
+    //例外処理
+    try {
+        // DBへ接続
+        $dbh = dbConnect();
+        // 件数用のSQL文作成
+        $sql = 'SELECT id FROM memo';
+        $data = array();
+        // クエリ実行
+        $stmt = queryPost($dbh, $sql, $data);
+        $rst['total'] = $stmt->rowCount(); //総レコード数
+    $rst['total_page'] = ceil($rst['total']/$listSpan); //総ページ数
+    if (!$stmt) {
+        return false;
+    }
+        // ページング用のSQL文作成
+        $sql = 'SELECT * FROM memo WHERE user_id = :u_id ORDER BY id DESC';
+        $sql .= ' LIMIT :listSpan OFFSET :currentMinNum';
+        debug('SQL：'.$sql);
+
+        //クエリー作成
+        $stmt = $dbh->prepare($sql);
+
+        $stmt->bindValue(':listSpan', $listSpan, PDO::PARAM_INT);
+        $stmt->bindValue(':currentMinNum', $currentMinNum, PDO::PARAM_INT);
+        $stmt->bindValue(':u_id', $u_id, PDO::PARAM_INT);
+        //プレースホルダに値をセットし、SQL文を実行
+        if (!$stmt->execute()) {
+            debug('クエリに失敗しました。');
+            debug('失敗したSQL：'.print_r($stmt, true));
+            $err_msg['common'] = MSG07;
+            return 0;
+        }
+        debug('クエリ成功。');
+        return $stmt;
+
+        if ($stmt) {
+            // クエリ結果のデータを全レコードを格納
+            $rst['data'] = $stmt->fetchAll();
+            return $rst;
+        } else {
+            return false;
+        }
+    } catch (Exception $e) {
+        error_log('エラー発生:' . $e->getMessage());
+    }
+}
